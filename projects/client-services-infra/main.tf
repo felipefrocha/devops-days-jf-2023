19d3cbe467dad16eaa21cc7a1ed909249957c2da
felipefrocha <felipefonsecarocha@gmail.com>
2023-05-19 01:12:19 -0300

    feat: fake commit 2

--- projects/client-services-infra/main.tf
+++ projects/client-services-infra/main.tf
@@ -8,6 +8,18 @@ terraform {
     }
   }
 }
-module "base-infra"{
 
+module "base-infra" {
+  source                 = "../../terraform/blueprints/base-infra"
+  environment            = var.environment
+  region                 = var.region
+  project                = var.project
+  server_name            = var.server_name
+  organization_name      = var.organization_name
+  download_certs         = var.download_certs
+  hosted_zone            = var.hosted_zone
+  route53_by_environment = var.route53_by_environment
+  access_key             = var.access_key
+  secret_key             = var.secret_key
+  token                  = var.token
 }
